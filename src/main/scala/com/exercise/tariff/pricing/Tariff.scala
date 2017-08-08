6cf84c54b4f1e1e2b484f430b220086d7d5df52e
Chris Smith <chris.smith@ovoenergy.com>
2017-08-08 17:25:07 +0100

    Make power and gas rates optional. Add parsing tests.

--- src/main/scala/com/exercise/tariff/pricing/Tariff.scala
+++ src/main/scala/com/exercise/tariff/pricing/Tariff.scala
@@ -1,3 +1,3 @@
 package com.exercise.tariff.pricing
 
-case class Tariff(tariff: String, standing_charge: Double)
+case class Tariff(tariff: String, rates: Rates, standing_charge: Double)
--- src/test/scala/com/exercise/tariff/pricing/ParseTests.scala
+++ src/test/scala/com/exercise/tariff/pricing/ParseTests.scala
@@ -11,7 +11,15 @@ class ParseTests extends FunSpec with Matchers {
                    |  {"tariff": "greener-energy", "rates": {"power":  0.1544}, "standing_charge": 8.33},
                    |  {"tariff": "simpler-energy", "rates": {"power":  0.1396, "gas": 0.0328}, "standing_charge": 8.75}
                    |]""".stripMargin
+
+      val tariffs = parse(json)
+
       parse(json).size should be(4)
+
+      tariffs(0) should equal(Tariff("better-energy", Rates(Some(0.1367), Some(0.0288)), 8.33))
+      tariffs(1) should equal(Tariff("2yr-fixed", Rates(Some(0.1397), Some(0.0296)), 8.75))
+      tariffs(2) should equal(Tariff("greener-energy", Rates(Some(0.1544), None), 8.33))
+      tariffs(3) should equal(Tariff("simpler-energy", Rates(Some(0.1396), Some(0.0328)), 8.75))
     }
   }
 }
